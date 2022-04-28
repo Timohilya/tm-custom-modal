cb98c937b958379370a757635a670b96e10ecd2f
Timohilya <63109870+Timohilya@users.noreply.github.com>
2022-04-28 13:01:20 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,55 +1,76 @@
 const Modal = {
     $modalButtons: document.querySelectorAll('[data-popupbtn]'),
+    $modals: document.querySelectorAll('[data-popup]'),
     init: function() {
+        window.modalsData = {};
+        if(this.$modals.length) {
+            this.$modals.forEach(popup => {
+                let name = popup.getAttribute('data-popup');
+                window.modalsData[name] = {};
+                window.modalsData[name].isOpen = false;
+            });
+        }
         if(this.$modalButtons.length) {
             this.$modalButtons.forEach(popupbtn => {
                 let name = popupbtn.getAttribute('data-popupbtn');
-                this.openPopupByButtonClick(name);
-                this.closePopupByOuterClick(name);
-                this.closePopupByButtonClick(name);
+                popupbtn.addEventListener('click', () => {
+                    document.dispatchEvent(new Event(name + 'Opened'));
+                    window.openPopup(name);
+                    window.modalsData[name].isOpen = true;
+                });
             });
         }
-    },
-    openPopupByButtonClick: function(name) {
-        document.querySelector(`[data-popupbtn=${name}]`).addEventListener('click', () => {
-            this.openPopupByName(name);
-        });
-    },
-    closePopupByOuterClick: function(name) {
-        document.querySelector(`[data-popup=${name}]`).addEventListener('click', e => {
-            if(e.target.classList.contains('modal__content')) {
-                this.closePopupByName(name);
+        if(this.$modals.length) {
+            this.$modals.forEach(popup => {
+                let name = popup.getAttribute('data-popup');
+                popup.addEventListener('click', e => {
+                    if(e.target.classList.contains('modal__content') || e.target.classList.contains('modal__close') || e.target.classList.contains('modal-close') ) {
+                        document.dispatchEvent(new Event(name + 'Closed'));
+                        window.closePopup(name);
+                        window.modalsData[name].isOpen = false;
+                    }
+                });
+            });
+        }
+
+        window.activePopups = [];
+
+        window.openPopup = function(name) {
+            if ( !window.activePopups.length ) {
+                window.pageScrollLock();
             }
-        });
-    },
-    closePopupByButtonClick: function(name) {
-        document.querySelector(`[data-popup=${name}]`).querySelector(`.modal__close`).addEventListener('click', () => {
-            this.closePopupByName(name);
-        });
-    },
-    openPopupByName: function(name) {
-        this.pageScrollLock();
-        document.querySelector(`[data-popup=${name}]`).classList.add('show');
-        document.querySelector(`[data-popup=${name}]`).classList.add('fade-in');
-    },
-    closePopupByName: function(name) {
-        this.pageScrollAvailable();
-        document.querySelector(`[data-popup=${name}]`).classList.add('fade-out');
-        setTimeout(() => {
-            document.querySelector(`[data-popup=${name}]`).classList.remove('show');
-            document.querySelector(`[data-popup=${name}]`).classList.remove('fade-in');
-            document.querySelector(`[data-popup=${name}]`).classList.remove('fade-out');
-        }, 300)
-    },
-    pageScrollLock: function() {
-        console.log(window.innerWidth, document.documentElement.clientWidth);
-        document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + 'px';
-        document.body.classList.add('scrollock');
-    },
-    pageScrollAvailable: function() {
-        document.body.style.paddingRight = 0;
-        document.body.classList.remove('scrollock');
+            window.activePopups.push(name);
+            document.querySelector(`[data-popup=${name}]`).classList.add('show');
+            document.querySelector(`[data-popup=${name}]`).classList.add('fade-in');
+        };
+
+        window.closePopup = function(name) {
+            window.activePopups = window.activePopups.filter(function(e) { return e !== name })
+            if ( !window.activePopups.length ) {
+                window.pageScrollAvailable();
+            }
+            document.querySelector(`[data-popup=${name}]`).classList.add('fade-out');
+            setTimeout(() => {
+                document.querySelector(`[data-popup=${name}]`).classList.remove('show');
+                document.querySelector(`[data-popup=${name}]`).classList.remove('fade-in');
+                document.querySelector(`[data-popup=${name}]`).classList.remove('fade-out');
+            }, 300)
+        };
+
+        window.pageScrollLock = function() {
+            //document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + 'px';
+            let scrollWidth = document.querySelector('body').offsetWidth - document.querySelector('body main').offsetWidth + 'px';
+            document.body.style.paddingRight = scrollWidth;
+            document.querySelector('.header').style.paddingRight = scrollWidth;
+            document.body.classList.add('scrollock');
+        };
+
+        window.pageScrollAvailable = function() {
+            document.body.style.paddingRight = 0;
+            document.querySelector('.header').style.paddingRight = 0;
+            document.body.classList.remove('scrollock');
+        };
     },
 }
 
-Modal.init();
+export default Modal;
